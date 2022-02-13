2edb196593d5c2c4f6038bfb09f8afe41d510639
febri4n <solihinfebrian0@gmail.com>
2022-02-13 11:23:35 +0700

    Fix broken image logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import reactLogo from "../assets/react-icon-small.png"
 
 export default function Header() {
   return (
     <nav>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/270px-React-icon.svg.png" className="nav--icon" alt="react logo"/>
+        <img src={reactLogo} className="nav--icon" alt="react logo"/>
         <h3 className="nav--logo_text">ReactFacts</h3>
         <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
